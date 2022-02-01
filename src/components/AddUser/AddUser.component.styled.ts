54813294bd208c74fda72d49d47bbc03d0812db9
SamuelSsc <samuelssc5874@gmail.com>
2022-02-01 18:26:53 -0300

    creating pageUserADD 1.0

--- src/components/AddUser/AddUser.component.styled.ts
+++ src/components/AddUser/AddUser.component.styled.ts
@@ -1,1 +1,43 @@
-import styled from 'styled-components'; 
+import styled from 'styled-components';
+
+export const Forms = styled.form `
+    display: flex;
+    flex-direction: column;
+    text-align: center;  
+    margin: auto;
+    width: 60vw;
+    align-items: center;  
+`;
+
+export const Input = styled.input `
+justify-content: center;
+text-align: center;
+border-radius: 10px;
+width: 85%; 
+height: 40px;
+
+`;
+
+export const Label = styled.label `
+align-self: flex-start;
+margin-top: 30px;
+margin-left: 20px;
+`;
+
+export const Button = styled.button `
+justify-content: center;
+border-radius: 10px;
+margin-top: 50px;
+background: #ad4dfc;
+color: white;
+border-style: none ;
+height: 50px;
+width: 100px;
+font: bold 15px arial, sans-serif;   
+`;
+
+export const Inputdata = styled.input`
+border-radius: 10px;
+width: 150px; 
+height: 25px;
+`
--- src/components/AddUser/AddUser.component.tsx
+++ src/components/AddUser/AddUser.component.tsx
@@ -1,6 +1,69 @@
 import React from "react";
+import { Button, Forms, Input, Inputdata, Label } from "./AddUser.component.styled";
 
 
 export const AddUser = () =>{
-    return  <h1>Hello Word</h1>
+    return  (
+
+        <Forms >          
+            <h3>Crie Uma Conta</h3>
+
+
+            <Label >Name:</Label>
+            <Input
+                placeholder="Digite seu nome" 
+                type="email"
+                required> 
+            </Input>
+
+            <Label >Telefone:</Label>
+            <Input
+                placeholder="Ex: (DDD)94002-8922" 
+                type="phone"
+                required> 
+            </Input>
+
+            <Label >E-mail:</Label>
+            <Input
+                name= "email"
+                placeholder="Email@example.com.br" 
+                type="email"
+                required     
+                pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
+                title="Digite um endereço de email valido. (NOTA:com letras minusculas)"> 
+            </Input>
+        
+            <Label>Senha:</Label>
+            <Input 
+                name= "Password"
+                placeholder="Digite sua Senha" 
+                type="password"
+                required
+                pattern= "(^(?=.*\d)(?=.*[a-zA-Z]).{7,}$)"
+                title="Sua Senha deve possuir no minimo 7 caracteres, com pelo menos 1 letra e 1 numero">
+            </Input>
+
+            <Label>Confirme sua Senha:</Label>
+            <Input 
+                name= "Password"
+                placeholder="Digite sua Senha" 
+                type="password"
+                required
+                pattern= "(^(?=.*\d)(?=.*[a-zA-Z]).{7,}$)"
+                title="Sua Senha deve possuir no minimo 7 caracteres, com pelo menos 1 letra e 1 numero">
+            </Input>
+
+        
+            <Label>Data de Nascimento</Label>
+            <Inputdata 
+                type="date"
+                required>
+            </Inputdata>
+        
+            <Button type="submit">Cadastrar</Button>
+    
+    
+        </Forms>
+    )
+    
 }
